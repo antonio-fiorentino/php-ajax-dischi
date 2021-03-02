7dc2f479fd1123697b8d28829d79996dbd2eb895
Antonio <antoniofiorentino16@gmail.com>
2021-03-02 19:11:51 +0100

    Esercizio

---
+++ json.php
@@ -0,0 +1,77 @@
+<?php
+
+$album = [
+  [
+    'poster' =>  'https://img.discogs.com/CrDQ3n2DlICAsWhKHB6BichPrDE=/fit-in/600x524/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4407037-1567219126-9335.jpeg.jpg',
+    'name'=> 'Lose yourself',
+    'artist' => 'Eminem',
+    'year'=> '2002'
+  ],
+
+  [
+    'poster' => 'https://i.pinimg.com/originals/c0/2e/63/c02e635b1f8e0953a763c4e4384c7880.jpg',
+    'name' => 'Welcome to jamrock',
+    'artist' => 'Damian Marley',
+    'year' => '2005'
+  ],
+
+  [
+    'poster' => 'https://m.media-amazon.com/images/I/914Bm5aIAoL._SS500_.jpg',
+    'name' => 'Life by the drop',
+    'artist' => 'Stevie Ray Vaughan',
+    'year' => '1991'
+  ],
+
+  [
+    'poster' => 'https://i1.sndcdn.com/artworks-nJzSG1tGb5OC-0-t500x500.jpg',
+    'name' => 'On sait Jamais',
+    'artist' => 'PLK',
+    'year' => '2020'
+  ],
+
+  [
+    'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2020/10/napoli-51-album-cover-nicola-siciliano.jpg',
+    'name' => 'Collera',
+    'artist' => 'Nicola Siciliano',
+    'year' => '2020'
+  ]
+
+  [
+    'poster' => '',
+    'name' => '',
+    'artist' => '',
+    'year' => ''
+  ],
+
+  [
+    'poster' => '',
+    'name' => '',
+    'artist' => '',
+    'year' => ''
+  ],
+
+  [
+    'poster' => '',
+    'name' => '',
+    'artist' => '',
+    'year' => ''
+  ],
+
+  [
+    'poster' => '',
+    'name' => '',
+    'artist' => '',
+    'year' => ''
+  ],
+
+  [
+    'poster' => '',
+    'name' => '',
+    'artist' => '',
+    'year' => ''
+  ],
+
+];
+
+var_dump($album);
+// $json = json_encode($album)
