934854b7b4486de32f8221533b38fd06be0d2f02
Antonio <antoniofiorentino16@gmail.com>
2021-03-03 16:19:04 +0100

    Esercizio

--- database/database.php
+++ database/database.php
@@ -6,70 +6,80 @@ $dischi = [
     'poster' =>  'https://img.discogs.com/CrDQ3n2DlICAsWhKHB6BichPrDE=/fit-in/600x524/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4407037-1567219126-9335.jpeg.jpg',
     'name'=> 'Lose yourself',
     'artist' => 'Eminem',
-    'year'=> '2002'
+    'year'=> '2002',
+    'genre' => 'rap'
   ],
 
   [
     'poster' => 'https://i.pinimg.com/originals/c0/2e/63/c02e635b1f8e0953a763c4e4384c7880.jpg',
     'name' => 'Welcome to jamrock',
     'artist' => 'Damian Marley',
-    'year' => '2005'
+    'year' => '2005',
+    'genre' => 'reggae'
   ],
 
   [
     'poster' => 'https://m.media-amazon.com/images/I/914Bm5aIAoL._SS500_.jpg',
     'name' => 'Life by the drop',
     'artist' => 'Stevie Ray Vaughan',
-    'year' => '1991'
+    'year' => '1991',
+    'genre' => 'blues'
   ],
 
   [
     'poster' => 'https://i1.sndcdn.com/artworks-nJzSG1tGb5OC-0-t500x500.jpg',
     'name' => 'On sait Jamais',
     'artist' => 'PLK',
-    'year' => '2020'
+    'year' => '2020',
+    'genre' => 'rap'
   ],
 
   [
     'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2020/10/napoli-51-album-cover-nicola-siciliano.jpg',
     'name' => 'Collera',
     'artist' => 'Nicola Siciliano',
-    'year' => '2020'
+    'year' => '2020',
+    'genre' => 'rap'
   ],
 
   [
     'poster' => 'https://www.hano.it/wp-content/uploads/2018/12/Murubutu-Tenebra-e-la-notte.jpg',
     'name' => 'Wordsworth',
     'artist' => 'Murubutu',
-    'year' => '2019'
+    'year' => '2019',
+    'genre' => 'rap'
   ],
 
   [
     'poster' => 'https://i1.sndcdn.com/artworks-000539200719-o3u5wj-t500x500.jpg',
     'name' => 'Sensimilia',
     'artist' => 'Collie Budz',
-    'year' => '2007'
+    'year' => '2007',
+    'genre' => 'reggae'
   ],
 
   [
     'poster' => 'https://www.viaggiandosimpara.org/wp-content/uploads/2016/09/lacio-drom-litfiba-02.jpg',
     'name' => 'Lacio drom',
     'artist' => 'Litfiba',
-    'year' => '1995'
+    'year' => '1995',
+    'genre' => 'rock'
   ],
 
   [
     'poster' => 'https://www.hano.it/wp-content/uploads/2019/05/Logic-Confessions-of-a-Dangerous-Mind.jpg',
     'name' => 'COMMANDO',
     'artist' => 'Logic',
-    'year' => '2019'
+    'year' => '2019',
+    'genre' => 'rap'
   ],
 
   [
     'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61pp4a-sv3L._AC_SX569_.jpg',
     'name' => 'Eroe',
     'artist' => 'Caparezza',
-    'year' => '2008'
+    'year' => '2008',
+    'genre' => 'rap'
   ],
 
 ];
--- index.php
+++ index.php
@@ -5,9 +5,41 @@
 // per la prima milestone
 require_once __DIR__ . '/database/database.php';
 
-print_r($dischi);
+// print_r($dischi);
+?>
+<!-- // qui a questo punto possiamo creare (per la prima milestone) tutto il nostro html e ciclare i nostri dischi (sempre in php)
+// e stampare copertina, titolo e così via, come da screenshot. -->
 
-// qui a questo punto possiamo creare (per la prima milestone) tutto il nostro html e ciclare i nostri dischi (sempre in php)
-// e stampare copertina, titolo e così via, come da screenshot.
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Album</title>
+    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
 
+    <div class="container">
+      <?php foreach ($dischi as $disco) {?>
+        <div class="">
+          <div class="">
+            <img src="<?php echo $disco['poster']?>">
+          </div>
+          <div class="">
+            <h2><?php echo $disco['name']?></h2>
+          </div>
+          <div class="">
+            <h3><?php echo $disco['artist']?></h3>
+          </div>
+          <div class="">
+            <h4><?php echo $disco['year']?></h4>
+          </div>
+        </div>
+      <?php}?>
+
+    </div>
+
+  </body>
+</html>
